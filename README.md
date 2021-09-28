b186a895c5c8948b79a13a21944e4bbfd31c2b50
Nisa Phelps <nisaphelps78@gmail.com>
2021-09-28 17:51:55 -0400

    Initial commit. Setup Django, Bootstrap, CSS connections, and two pages. Added example HTML, CSS, and Bootstrap.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Database Project
+## Background
+A group project for COP4710 Database Design.
+## Tools
+ - Django
---
+++ db_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class dbAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'db_app'
