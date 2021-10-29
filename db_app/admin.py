a68cafc8ab507ba40a16c1d56c67d03f6dd3f53c
Nisa Phelps <nisaphelps78@gmail.com>
2021-10-29 19:57:01 -0400

    Created initial database

--- db_app/admin.py
+++ db_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from db_app import models
 
-# Register your models here.
+admin.site.register(models.Item)
+admin.site.register(models.Customer)
+admin.site.register(models.Subscriber)
+admin.site.register(models.SubOnlyItem)
+admin.site.register(models.WeeklyAd)
+admin.site.register(models.PreordersSubItem)
+admin.site.register(models.BuysSubItem)
+admin.site.register(models.Buys)
