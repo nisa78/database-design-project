16951e73949845d3e2ca43d31749993b0bca0fb5
Nisa Phelps <nisaphelps78@gmail.com>
2021-10-29 20:52:22 -0400

    Added ability to see the weekly ad's content

--- db_app/views.py
+++ db_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from db_app import models
 
 def home(request):
-  return render(request, 'db_project/home.html')
+  weekly_ads = models.WeeklyAd.objects.all()
+  return render(request, 'db_project/home.html', {"weekly_ads": weekly_ads})
 def about(request):
   return render(request, 'db_project/about.html')
